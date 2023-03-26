383b644f803d72d7a3a757382257617c40d91db5
VinsMasciullo <mvincy@gmail.com>
2023-03-26 19:06:20 +0200

    Update application.py

--- application.py
+++ application.py
@@ -23,25 +23,25 @@ datiClean = np.where(arrayCompleto == 'Rifiuti in vetro')
 #nella prima linea del file troviamo le date, e rimuovo il primo elemento
 date_finali = [int(numeric_string) for numeric_string in arrayCompleto[0][1:]]
 
-#estraggo i valori della riga corrispondente al vetro pre-cedentemente identificata
+#estraggo i valori della riga corrispondente al vetro precedentemente identificata
 valori = arrayCompleto[datiClean[0][0]][1:]
 
 #faccio un ulteriore check e faccio il casting float
 valori_float = valori.astype(float)
 
-#per ogni elemento presente nell’array valori finali, fac-cio una trasformazione in grammi e divido per il peso del-la bottiglia 
-valori_finali = [round(B *1000000/ pesoBott) for B in va-lori_float]
+#per ogni elemento presente nell’array valori finali, faccio una trasformazione in grammi e divido per il peso della bottiglia 
+valori_finali = [round(B *1000000/ pesoBott) for B in valori_float]
 valori_economici_finali = [(B * valoreBottTonn) for B in valori_float]
 
 #vado a definire un range del dataframe num bottiglie
-tabellaNumBott = {'anno': date_finali, 'nume-ro_bottiglie': valori_finali}
+tabellaNumBott = {'anno': date_finali, 'numero_bottiglie': valori_finali}
 dataFrameNumBott = pd.DataFrame(data= tabellaNumBott)
 
 #stampo a video una tabella con i contenuti num bottiglie
 dataFrameNumBott
 
 #vado a definire un range del dataframe euro 
-tabellaCompEcon = {'anno': date_finali, 'Euro guadagna-ti': valori_economici_finali}
+tabellaCompEcon = {'anno': date_finali, 'Euro guadagnati': valori_economici_finali}
 dataFrameCompEcon = pd.DataFrame(data= tabellaCompEcon)
 
 #stampo a video una tabella del range identificato
@@ -58,10 +58,10 @@ l1, = ax1.plot(date_finali ,valori_finali, color='red')
 #utilizzo entrambi gli assi delle ordinate
 ax2 = ax1.twinx()
 
-ax2.set_ylabel("Milioni di Euro Guadagnati", co-lor='green', fontsize=14)
+ax2.set_ylabel("Milioni di Euro Guadagnati", color='green', fontsize=14)
 ax2.tick_params(axis="y", labelcolor='green')
 
-l2, = ax2.plot(date_finali ,valori_economici_finali, co-lor='green')
+l2, = ax2.plot(date_finali ,valori_economici_finali, color='green')
 
 # faccio visualizzare il grafico 
 plt.show()
